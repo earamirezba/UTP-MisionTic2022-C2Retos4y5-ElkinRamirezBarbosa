6c4ecf588d02fa7b0955490bd5c7ecc213d4d285
earamirezba <elkinramirezbarbosa@gmail.com>
2021-08-16 07:13:47 -0500

    initial commit

---
+++ src/main/java/com/utp/misiontic2022/c2reto4y5/elkin/controller/ReportesController.java
@@ -0,0 +1,5 @@
+package com.utp.misiontic2022.c2reto4y5.elkin.controller;
+
+public class ReportesController {
+    
+}
---
+++ src/main/java/com/utp/misiontic2022/c2reto4y5/elkin/view/ReportesView.java
@@ -0,0 +1,5 @@
+package com.utp.misiontic2022.c2reto4y5.elkin.view;
+
+public class ReportesView {
+    
+}
