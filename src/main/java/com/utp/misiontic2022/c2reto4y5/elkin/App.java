723422f91f70a616e8015fa1e52b02e83d8ded0e
earamirezba <elkinramirezbarbosa@gmail.com>
2021-08-16 07:42:08 -0500

    1st task ReportesView

--- src/main/java/com/utp/misiontic2022/c2reto4y5/elkin/App.java
+++ src/main/java/com/utp/misiontic2022/c2reto4y5/elkin/App.java
@@ -1,13 +1,15 @@
 package com.utp.misiontic2022.c2reto4y5.elkin;
 
+import com.utp.misiontic2022.c2reto4y5.elkin.view.ReportesView;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        var reportesView = new ReportesView();
+        var banco = "Conavi";
+        reportesView.proyectosFinanciadosPorBanco(banco);
     }
 }
