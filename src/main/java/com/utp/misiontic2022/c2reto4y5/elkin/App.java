470412a9df2b0d4d147beebc034978b75bafa668
earamirezba <elkinramirezbarbosa@gmail.com>
2021-08-16 07:15:44 -0500

    initial commit

---
+++ src/main/java/com/utp/misiontic2022/c2reto4y5/elkin/App.java
@@ -0,0 +1,13 @@
+package com.utp.misiontic2022.c2reto4y5.elkin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/utp/misiontic2022/c2reto4y5/elkin/AppTest.java
@@ -0,0 +1,20 @@
+package com.utp.misiontic2022.c2reto4y5.elkin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
